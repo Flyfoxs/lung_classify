2ab8aca0fb852e920acfec47d07dedccf69647ec
lilao <lilao@163.com>
2020-02-26 17:14:54 +0800

    ensemble stable version

---
+++ bin/ens.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+cd ..
+
+mkdir -p log
+pwd
+search_dir=configs
+for conf_file in "$search_dir"/ens*.yaml
+do
+  for fold in {0..4}
+  do
+    CUDA_VISIBLE_DEVICES=$1 && python -u customer/classify_ensemble.py main with conf_name=$conf_file fold=$fold  >> ./log/ens_"$(hostname)"_$1.log 2>&1
+    #python -u core/train_lgb.py train -1 $fold >> log/lgb_m_$fold.log 2>&1
+  done
+done
+
+#CUDA_VISIBLE_DEVICES=3  &&   python train.py --config resources/train_config_ce.yaml
+
+
+
--- customer/classify_ensemble.py
+++ customer/classify_ensemble.py
@@ -3,6 +3,14 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import shutil
+from fastai import *
+from fastai.vision import *
+from fastai.widgets import *
+from fastai.vision.learner import cnn_config
+from fastai.callbacks.hooks import num_features_model
+
+
+from task_distribute.locker import task_locker
 from torchvision import transforms
 from torchvision import models
 import torch
@@ -15,12 +23,13 @@ from torchvision.datasets import ImageFolder
 from torchvision.utils import make_grid
 from torch.utils.data import Dataset,DataLoader
 import time
-from tqdm import tqdm_notebook as tqdm
+from tqdm import tqdm
 
 import os
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+
 is_cuda = True
+version = 'en02'
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -28,7 +37,9 @@ from PIL import Image as plt_img
 import cv2
 from torchvision.datasets.folder import default_loader
 from albumentations.pytorch.functional import img_to_tensor
-
+from sacred import Experiment
+from easydict import EasyDict as edict
+from sacred.observers import MongoObserver
 
 class LungDataset(Dataset):
 
@@ -74,33 +85,32 @@ class LungDataset(Dataset):
         label = self.df.iloc[idx, 1]
         return sample, label
 
+def get_dl(valid_fold,image_size):
+    valid_fold = int(valid_fold)
+    transform = transforms.Compose([
+        transforms.Resize([image_size, image_size]),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+        # transforms.RandomErasing(),
+    ])
 
-transform = transforms.Compose([
-    transforms.Resize([256, 256]),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
-    # transforms.RandomErasing(),
-])
+    train_folds = list(range(5))
+    train_folds.remove(valid_fold)
+    train = LungDataset('./input/train.csv', './input/train/', fold=train_folds, transform=transform)
 
-train = LungDataset('./input/train.csv', './input/train/', fold=(0, 1, 2, 3), transform=transform)
+    valid = LungDataset('./input/train.csv', './input/train/', fold=(valid_fold,), transform=transform)
 
-valid = LungDataset('./input/train.csv', './input/train/', fold=(4,), transform=transform)
+    # test = ImageFolder('/home/felix/pj/lung_classify/input/')
 
-# test = ImageFolder('/home/felix/pj/lung_classify/input/')
+    print(len(train), len(valid))
+    # , len(test)
 
-len(train), len(valid)
-# , len(test)
+    train_data_loader = torch.utils.data.DataLoader(train,batch_size=16,num_workers=3,shuffle=True)
+    valid_data_loader = torch.utils.data.DataLoader(valid,batch_size=8,num_workers=3,shuffle=True)
 
+    return train_data_loader, valid_data_loader
 
-train_data_loader = torch.utils.data.DataLoader(train,batch_size=16,num_workers=3,shuffle=True)
-valid_data_loader = torch.utils.data.DataLoader(valid,batch_size=8,num_workers=3,shuffle=True)
-
-from fastai import *
-from fastai.vision import *
-from fastai.widgets import *
-from fastai.vision.learner import cnn_config
-from fastai.callbacks.hooks import num_features_model
 
 
 def create_head(nf: int, ps: Floats = 0.5, concat_pool: bool = True, bn_final: bool = False):
@@ -146,18 +156,20 @@ def create_cnn_model(base_arch, cut: Union[int, Callable] = None, pretrained: bo
 #         bn_final=False, concat_pool=True)
 
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, subnet_list):
         super().__init__()
 
         from functools import partial
         create_backbone = partial(create_cnn_model, cut=None, pretrained=True, lin_ftrs=None, ps=0.5, custom_head=None, bn_final=False, concat_pool=True)
         #self.create_backbone(item).cuda() for item in [models.resnet34, models.densenet161, models.vgg19_bn] ]
 
-        self.model_res = create_backbone(models.resnet34)
+        if isinstance(subnet_list, str) : subnet_list = subnet_list.split(',')
 
-        self.model_dense = create_backbone(models.densenet161)
+        if 'resnet34' in subnet_list: self.model_res = create_backbone(models.resnet34)
 
-        self.model_vgg19_bn = create_backbone(models.vgg19_bn)
+        if 'densenet161' in subnet_list: self.model_dense = create_backbone(models.densenet161)
+
+        if 'vgg19_bn' in subnet_list: self.model_vgg19_bn = create_backbone(models.vgg19_bn)
 
         self.fc3 = nn.Linear(128, 5)
 
@@ -181,17 +193,6 @@ class Net(nn.Module):
         return F.log_softmax(x, dim=1)
 
 
-model = Net()
-if is_cuda:
-    model.cuda()
-
-print(model)
-
-print(model._modules)
-
-optimizer = optim.SGD(model.parameters(),lr=0.01,momentum=0.5)
-
-
 def fit(epoch, model, data_loader, phase='training', volatile=False):
     if phase == 'training':
         model.train()
@@ -200,6 +201,7 @@ def fit(epoch, model, data_loader, phase='training', volatile=False):
         volatile = True
     running_loss = 0.0
     running_correct = 0
+    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
     for batch_idx, (data, target) in tqdm(enumerate(data_loader), desc=f'{epoch}:{phase}', total=len(data_loader)):
         if is_cuda:
             data, target = data.cuda(), target.cuda()
@@ -221,8 +223,8 @@ def fit(epoch, model, data_loader, phase='training', volatile=False):
             loss.backward()
             optimizer.step()
 
-    loss = running_loss / len(data_loader.dataset)
-    accuracy = 100. * running_correct / len(data_loader.dataset)
+    loss = float(running_loss / len(data_loader.dataset))
+    accuracy = float(100. * running_correct / len(data_loader.dataset))
 
     print(
         f'{phase} loss is {loss:{5}.{2}} and {phase} accuracy is {running_correct}/{len(data_loader.dataset)}{accuracy:{10}.{4}}')
@@ -230,30 +232,114 @@ def fit(epoch, model, data_loader, phase='training', volatile=False):
     return loss, accuracy
 
 
-train_losses , train_accuracy = [],[]
-val_losses , val_accuracy = [],[]
-
-patient = 3
-best_epoch = 0
-for epoch in range(1,30):
-    epoch_loss, epoch_accuracy = fit(epoch,model,train_data_loader,phase='training')
-    val_epoch_loss , val_epoch_accuracy = fit(epoch,model,valid_data_loader,phase='validation')
-    train_losses.append(epoch_loss)
-    train_accuracy.append(epoch_accuracy)
-    val_losses.append(val_epoch_loss)
-    val_accuracy.append(val_epoch_accuracy)
-
-    best_score = np.max(val_accuracy)
-    if val_epoch_accuracy == best_score:
-        best_epoch = epoch
-        wait = 0
-    else:
-        wait = wait + 1
-
-    if wait >= patient:
-        print(f'Early stop wait {wait} epochs, and current epoch:{epoch}, best:{best_score:6.5f}/{best_epoch:02} ')
 
 
-"""" 
-python -u customer/classify_ensemble.py main with conf_name=5cls_resnet34  fold=0
-"""
+
+###### sacred begin
+from sacred import Experiment
+from easydict import EasyDict as edict
+from sacred.observers import MongoObserver
+from sacred import SETTINGS
+
+ex = Experiment('lung')
+db_url = 'mongodb://sample:password@10.10.20.103:27017/db?authSource=admin'
+ex.observers.append(MongoObserver(url=db_url, db_name='db'))
+#SETTINGS.CAPTURE_MODE = 'sys'
+
+@ex.config
+def my_config():
+    conf_name = None
+    fold = -1
+
+
+# @ex.command()
+# def main(_config):
+#
+#     config = edict(_config)
+#     conf_name = config.conf_name
+#     valid_fold = int(config.fold)
+#
+#     train(valid_fold, conf_name)
+
+
+@ex.command()
+def main(_config):
+    config = edict(_config)
+
+    file_name = config.conf_name
+    if not os.path.exists(file_name):
+        print(f'Can not find file:{file_name}')
+        file_name = f'./configs/{file_name}.yaml'
+    f = open(file_name)
+    conf = edict(yaml.load(f))
+
+    model = Net(conf.backbone)
+    valid_fold = int(config.fold)
+
+
+    model.cuda()
+
+    print(model)
+
+    train_losses, train_accuracy = [], []
+    val_losses, val_accuracy = [], []
+
+    patient = 3
+    best_epoch = 0
+
+    train_data_loader, valid_data_loader = get_dl(valid_fold, conf.image_size)
+    print(conf)
+    for epoch in tqdm(range(1, 30), desc='epoch'):
+        epoch_loss, epoch_accuracy = fit(epoch, model, train_data_loader, phase='training')
+        ex.log_scalar('train.acc', epoch_accuracy, step=epoch)
+        ex.log_scalar('train.loss', epoch_loss, step=epoch)
+
+        val_epoch_loss, val_epoch_accuracy = fit(epoch, model, valid_data_loader, phase='validation')
+        ex.log_scalar('val.loss', val_epoch_loss, step=epoch)
+        ex.log_scalar('val.acc', val_epoch_accuracy, step=epoch)
+
+        train_losses.append(epoch_loss)
+        train_accuracy.append(epoch_accuracy)
+        val_losses.append(val_epoch_loss)
+        val_accuracy.append(val_epoch_accuracy)
+
+        best_score = np.max(val_accuracy)
+        if val_epoch_accuracy == best_score:
+            best_epoch = epoch
+            wait = 0
+        else:
+            wait = wait + 1
+
+        if wait >= patient:
+            print(f'Early stop wait {wait} epochs, and current epoch:{epoch}, fold:{valid_fold}, best:{best_score:6.5f}/{best_epoch:02}, {file_name} ')
+            break
+
+    print('epoch_accuracy', epoch_accuracy )
+    print('val_epoch_accuracy', val_epoch_accuracy )
+
+if __name__ == '__main__':
+
+    """" 
+    python -u customer/classify_ensemble.py main with conf_name=ens_res_den_vgg  fold=0
+    """
+
+    from sacred.arg_parser import get_config_updates
+    import sys
+    config_updates, named_configs = get_config_updates(sys.argv[1:])
+    conf_name = config_updates.get('conf_name')
+    fold = config_updates.get('fold')
+
+
+    locker = task_locker('mongodb://sample:password@10.10.20.103:27017/db?authSource=admin', remove_failed =9 , version=version)
+    task_id = f'lung_{conf_name}_{fold}'
+    #pydevd_pycharm.settrace('192.168.1.101', port=1234, stdoutToServer=True, stderrToServer=True)
+    with locker.lock_block(task_id=task_id) as lock_id:
+        if lock_id is not None:
+            ex.add_config({
+                'lock_id': lock_id,
+                'lock_name': task_id,
+                'version': version,
+            })
+            res = ex.run_commandline()
+
+

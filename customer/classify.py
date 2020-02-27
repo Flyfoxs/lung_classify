7b59131b2e7354cb4e5db5a798e627910712a2e8
lilao <lilao@163.com>
2020-02-27 19:30:36 +0800

    better version with default transform

--- customer/classify.py
+++ customer/classify.py
@@ -123,20 +123,20 @@ def train(valid_fold , conf_name):
     # if class_cnt <= 2:
     #     df.label = np.where(df.label>=1, 1, 0)
 
-    def get_transforms(do_flip: bool = True, flip_vert: bool = False, max_rotate: float = 10., max_zoom: float = 1.1,
-                       max_lighting: float = 0.2, max_warp: float = 0.2, p_affine: float = 0.75,
-                       p_lighting: float = 0.75, xtra_tfms: Optional[Collection[Transform]] = None) -> Collection[Transform]:
-        "Utility func to easily create a list of flip, rotate, `zoom`, warp, lighting transforms."
-        res = [rand_crop()]
-        if do_flip:    res.append(dihedral_affine() if flip_vert else flip_lr(p=0.5))
-        if max_warp:   res.append(symmetric_warp(magnitude=(-max_warp, max_warp), p=p_affine))
-        if max_rotate: res.append(rotate(degrees=(-max_rotate, max_rotate), p=p_affine))
-        if max_zoom > 1: res.append(rand_zoom(scale=(1., max_zoom), p=p_affine))
-        if max_lighting:
-            res.append(brightness(change=(0.5 * (1 - max_lighting), 0.5 * (1 + max_lighting)), p=p_lighting))
-            res.append(contrast(scale=(1 - max_lighting, 1 / (1 - max_lighting)), p=p_lighting))
-        #       train                   , valid
-        return (res + listify(xtra_tfms)+zoom_crop(scale=0.1), zoom_crop(scale=0.1))
+    # def get_transforms(do_flip: bool = True, flip_vert: bool = False, max_rotate: float = 10., max_zoom: float = 1.1,
+    #                    max_lighting: float = 0.2, max_warp: float = 0.2, p_affine: float = 0.75,
+    #                    p_lighting: float = 0.75, xtra_tfms: Optional[Collection[Transform]] = None) -> Collection[Transform]:
+    #     "Utility func to easily create a list of flip, rotate, `zoom`, warp, lighting transforms."
+    #     res = [rand_crop()]
+    #     if do_flip:    res.append(dihedral_affine() if flip_vert else flip_lr(p=0.5))
+    #     if max_warp:   res.append(symmetric_warp(magnitude=(-max_warp, max_warp), p=p_affine))
+    #     if max_rotate: res.append(rotate(degrees=(-max_rotate, max_rotate), p=p_affine))
+    #     if max_zoom > 1: res.append(rand_zoom(scale=(1., max_zoom), p=p_affine))
+    #     if max_lighting:
+    #         res.append(brightness(change=(0.5 * (1 - max_lighting), 0.5 * (1 + max_lighting)), p=p_lighting))
+    #         res.append(contrast(scale=(1 - max_lighting, 1 / (1 - max_lighting)), p=p_lighting))
+    #     #       train                   , valid
+    #     return (res + listify(xtra_tfms)+zoom_crop(scale=0.1), zoom_crop(scale=0.1))
 
     data = (ImageList.from_df(df, './input/train/', )
              .split_by_idx(df.loc[df.fold == valid_fold].index)
